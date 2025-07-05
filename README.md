ef5b6d56c22018f1e72e104110a633ee8114fadf
giovak17 <kevin.giovanni1703@gmail.com>
2025-07-04 21:33:27 -0700

    login terminado

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * Crear un entorno virtual
 * Activar el entorno virtual
 * Instalar los paquetes con `pip install -r requirements.txt`
+* Modificar las credenciales de base de datos en `project/settings.py`
+* Ejectutar `python manage.py migrate`
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-# from core.models import Usuarios
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
+from core.models import Usuarios
 
 
 def index(request):
     return render(request, "usuarios/index.html")
+
+def login(request: HttpRequest): 
+    if request.method == "GET":
+        return render(request, "usuarios/login.html")
+
+    email = request.POST["email"]
+    password = request.POST["password"]
+
+    user = Usuarios.objects.filter(correo_electronico=email).first()
+
+    # Si el correo y contrasena son correctos, permite el accesso
+    if user and user.contrasena == password:
+        return redirect("usuarios:index")
+    
+    if not user:
+        message = "Correo electronico no registrado."
+    else:
+        if user.contrasena != password: 
+            message = "Contrasena incorrecta."
+
+
+    return render(request, "usuarios/login.html", {"message": message, "email": email})
+
+
+
+
