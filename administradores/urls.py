dc3c241a8c449ede7b116e09032e281a49f14707
America <0322103716@ut-tijuana.edu.mx>
2025-07-07 04:17:27 -0700

    Contendio educativo, entregas

--- administradores/urls.py
+++ administradores/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "administradores"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('administradores/recicladoras/', views.ver_recicladoras, name='ver_recicladoras'),
 ]
--- administradores/views.py
+++ administradores/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from core.models import Recicladoras
 
 # Create your views here.
 def index(request):
     return render(request, "administradores/index.html")
+def ver_recicladoras(request):
+    recicladoras = Recicladoras.objects.select_related('propietario').all()
+    return render(request, 'administradores/recicladoras.html', {'recicladoras': recicladoras})
