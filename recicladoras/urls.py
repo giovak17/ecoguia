1093994bb360db0606fdaa480f15d2b9b4bdd57c
arath <94558953+aramsesporcayomercado@users.noreply.github.com>
2025-07-22 15:22:48 -0700

    la url tambien

--- recicladoras/urls.py
+++ recicladoras/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('tipomaterial/registro/', views.tipo_material_registro, name='tipomaterial_registro'),
     path('tipomaterial/<int:pk>/update/', views.tipo_material_actualizar, name='tipomaterial_actualizar'),
     path('tipomaterial/<int:pk>/delete/', views.tipo_material_delete, name='tipomaterial_delete'),
+    path('clasificacion', views.clasificacion, name='clasificacion_materiales'),
 
     path('solicitar/', views.solicitar_recicladora, name='solicitar_recicladora'),
 ]
